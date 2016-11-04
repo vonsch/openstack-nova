openstack/nova

Fix service version to update the DB
When new code is installed with a newer service version, the version in
the DB should get updated.

The fix here is to:
1) eliminate the special case for 'version' in save()
2) cause version to be saved on startup

Change-Id: I96fa9dabfb9b7a5f1703baf80534d8b104dab4e6
Closes-Bug: 1579839

nova/tests/unit/test_service.py
@@ -157,6 +157,23 @@ class ServiceTestCase(test.NoDBTestCase):
                                'nova.tests.unit.test_service.FakeManager')
         serv.start()
 
+    @mock.patch('nova.objects.service.Service.get_by_host_and_binary')
+    def test_start_updates_version(self, mock_get_by_host_and_binary):
+        # test that the service version gets updated on services startup
+        service_obj = mock.Mock()
+        service_obj.binary = 'fake-binary'
+        service_obj.host = 'fake-host'
+        service_obj.version = -42
+        mock_get_by_host_and_binary.return_value = service_obj
+
+        serv = service.Service(self.host, self.binary, self.topic,
+                              'nova.tests.unit.test_service.FakeManager')
+        serv.start()
+
+        # test service version got updated and saved:
+        service_obj.save.assert_called_once()
+        self.assertEqual(objects.service.SERVICE_VERSION, service_obj.version)
+
     def _test_service_check_create_race(self, ex):
         self.manager_mock = self.mox.CreateMock(FakeManager)
         self.mox.StubOutWithMock(sys.modules[__name__], 'FakeManager',

vonsch/openstack-nova

NOTE(lyarwood): Single conflict caused by the use of Mock 1.3.0 on
stable/mitaka and as a result the lack of assert_called_once().
Conflicts:
        nova/tests/unit/test_service.py
(cherry picked from commit 854c39e26d94f006f539ea993ffaafa4892b100c)

nova/tests/unit/test_service.py
@@ -146,6 +146,23 @@ class ServiceTestCase(test.NoDBTestCase):
                                'nova.tests.unit.test_service.FakeManager')
         serv.start()
 
+    @mock.patch('nova.objects.service.Service.get_by_host_and_binary')
+    def test_start_updates_version(self, mock_get_by_host_and_binary):
+        # test that the service version gets updated on services startup
+        service_obj = mock.Mock()
+        service_obj.binary = 'fake-binary'
+        service_obj.host = 'fake-host'
+        service_obj.version = -42
+        mock_get_by_host_and_binary.return_value = service_obj
+
+        serv = service.Service(self.host, self.binary, self.topic,
+                              'nova.tests.unit.test_service.FakeManager')
+        serv.start()
+
+        # test service version got updated and saved:
+        service_obj.save.assert_called_once_with()
+        self.assertEqual(objects.service.SERVICE_VERSION, service_obj.version)
+
     def _test_service_check_create_race(self, ex):
         self.manager_mock = self.mox.CreateMock(FakeManager)
         self.mox.StubOutWithMock(sys.modules[__name__], 'FakeManager',